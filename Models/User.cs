310eef38e4b478d21005448f984fcec25c8bc745
Lucas Guidi <lucasrguidi@gmail.com>
2025-02-03 16:49:36 -0300

    feat: :technologist: create User model

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+namespace UserRegister.Models
+{
+    public class User
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public string CPF { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
