53455c2ba2177d09e087153b231cd37a336428ae
Lucas Guidi <lucasrguidi@gmail.com>
2025-02-03 16:55:11 -0300

    feat: :sparkles: created user controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using UserRegister.Models;
+
+namespace UserRegister.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        [HttpPost]
+        public IActionResult Salvar([FromBody] User user)
+        {
+            if (string.IsNullOrEmpty(user.Name) || string.IsNullOrEmpty(user.CPF))
+            {
+                return BadRequest("Nome e CPF são obrigatórios.");
+            }
+
+            string code = user.CPF.Substring(0, 4);
+            return Ok($"Pessoa cadastrada com sucesso, código: {code}");
+        }
+    }
+}
